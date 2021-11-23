7fdc754b1d3cdee9b2b721c129b500c441322247
Aditya Gupta <gupta.aditya@gmail.com>
2021-11-23 21:28:40 +0100

    firs

--- first.py
+++ first.py
@@ -1,1 +1,24 @@
 #first python script
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as num
+from scipy import stats
+
+full_health_data = pd.read_csv("data.csv", header=0, sep=",")
+
+x = full_health_data["Average_Pulse"]
+y = full_health_data["Calorie_Burnage"]
+
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+
+def myfunc(x):
+ return slope * x + intercept
+mymodel = list(map(myfunc, x))
+
+plt.scatter(x, y)
+plt.plot(x, mymodel)
+plt.ylim(ymin=0, ymax=2000)
+plt.xlim(xmin=0, xmax=200)
+plt.xlabel("Average_Pulse")
+plt.ylabel ("Calorie_Burnage")
+plt.show()
